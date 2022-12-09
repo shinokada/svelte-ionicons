1b5f0a804b332dac4a3b45cbe377297f6fc43f3f
Shinichi Okada <okada.shin@gmail.com>
2022-12-10 08:46:59 +0900

    fix: sveltekit update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 };
 
 export default config;
