43dc11bd199d8896ec99e34cb5987a522e09858f
Shinichi Okada <okada.shin@gmail.com>
2023-07-16 20:59:10 +0200

    style: update dark color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,8 +47,8 @@
   }}
 />
 
-<header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
-  <Navbar color="default" fluid let:hidden let:toggle class="dark:bg-slate-950 ">
+<header class="flex-none w-full mx-auto bg-white dark:bg-purple-950">
+  <Navbar color="default" fluid let:hidden let:toggle class="dark:bg-purple-950 ">
     <NavBrand href="/">
       <span
         class="self-center whitespace-nowrap text-2xl font-semibold text-primary-900 dark:text-primary-500"
@@ -82,7 +82,7 @@
   <slot />
 </div>
 
-<Footer footerType="logo" class="dark:bg-slate-950">
+<Footer footerType="logo" class="dark:bg-purple-950">
   <div class="sm:flex sm:items-center sm:justify-between">
     <FooterBrand
       href="https://svelte-ionicons.vercel.app/"
--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -18,7 +18,7 @@
     let n = (Math.random() * 0xfffff * 1000000).toString(16);
     return '#' + n.slice(0, 6);
   };
-  const contentClass = ' rounded-lg dark:bg-slate-950 mt-4';
+  const contentClass = ' rounded-lg dark:bg-purple-950 mt-4';
   let searchTerm = '';
 
   $: filteredEntries = Object.entries(Icons).filter(([name, component]) => {
