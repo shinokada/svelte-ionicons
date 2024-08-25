81979c9e6b54a2ca48c9d20f14b8df7de1a61384
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-08-25 17:37:40 +0200

    fix: svelte-5-ui-lib runes-webkit update

---
+++ src/routes/utils/helpers.ts
@@ -0,0 +1,17 @@
+import type { Component } from 'svelte';
+import type { ListType } from 'runes-webkit';
+import { ExpandOutline, CogOutline, sidebarList } from 'runes-webkit'
+
+const extra: ListType[] = [
+  {
+    name: 'Icons',
+    Icon: ExpandOutline as Component,
+    href: '/icons'
+  },
+  {
+    name: 'Icon Sets',
+    Icon: CogOutline as Component,
+    href: 'https://svelte-svg-icons.codewithshin.com/'
+  }
+];
+export const newSidebarList: ListType[] = [...sidebarList, ...extra];
